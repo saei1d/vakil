da84e0fd109a82e1d139f96107768a19bb3dc39d
khaneara <hehehe3bar@gmail.com>
2025-03-17 11:02:56 +0330

    asgi.settings

--- vakil/asgi.py
+++ vakil/asgi.py
@@ -12,7 +12,11 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter
 
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vakil.settings")
+
+
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    # اگر WebSocket داری، اینجا اضافه کن
+    "http": get_asgi_application()
 })
