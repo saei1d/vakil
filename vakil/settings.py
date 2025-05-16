01f914ec1d3d449972fcbaf4c9e3b3fede75b448
saei1d <hehehe3bar@gmail.com>
2025-05-16 20:45:30 +0330

    menu bug5

--- vakil/settings.py
+++ vakil/settings.py
@@ -187,7 +187,7 @@ USE_I18N = True
 import os
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/app/media'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
